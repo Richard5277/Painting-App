219fcf323c5b47c76effd0207249a4b28ae98c1e
Richard5277 <peter.hong92@icloud.com>
2016-10-16 16:39:24 -0400

    function ( StrokeWidth & Eraser )
    Basic function of choose stroke width & eraser

--- drawing/js/app.js
+++ drawing/js/app.js
@@ -1,29 +1,49 @@
-//Problem: No user interaction causes no change to application
-//Solution: When user interacts cause changes appropriately
+// Drawing Application
 var color = $(".selected").css("background-color");
+var strokeWidth;
 var $canvas = $("canvas");
+$canvas.addClass("penCurse");
 var context = $canvas[0].getContext("2d");
 var lastEvent;
 var mouseDown = false;
 
-//When clicking on control list items
+
 $(".controls").on("click", "li", function(){
-  //Deselect sibling elements
   $(this).siblings().removeClass("selected");
-  //Select clicked element
   $(this).addClass("selected");
-  //cache current color
   color = $(this).css("background-color");
 });
+
+$("#chooseStrokeWidth").on("click", function(){
+  strokeWidth = $("#inputStroke").val();
+});
+function removeAllClass(){
+  $canvas.removeClass("penCurse");
+  $canvas.removeClass("eraserCurse");
+  $canvas.removeClass("crayonCurse");
+}
+
+$(".tools").on("click", "li", function(){
+  $(this).siblings().removeClass("choosed");
+  $(this).addClass("choosed");
+
+  if ($(this).hasClass("pen")){
+    removeAllClass();
+    $canvas.addClass("penCurse");
+  } else if($(this).hasClass("eraser")){
+    removeAllClass();
+    $canvas.addClass("eraserCurse");
+  } else if ($(this).hasClass("crayon")){
+    removeAllClass();
+    $canvas.addClass("crayonCurse");
+  }
+});
   
-//When "New Color" is pressed
 $("#revealColorSelect").click(function(){
-  //Show color select or hide the color select
   changeColor();
   $("#colorSelect").toggle();
 });
 
-//update the new color span
 function changeColor() {
   var r = $("#red").val();
   var g = $("#green").val();
@@ -31,32 +51,37 @@ function changeColor() {
   $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
 }
 
-//When color sliders change
 $("input[type=range]").change(changeColor);
 
-//When "Add Color" is pressed
 $("#addNewColor").click(function(){
-  //Append the color to the controls ul
   var $newColor = $("<li></li>");
   $newColor.css("background-color", $("#newColor").css("background-color"));
   $(".controls ul").append($newColor);
-  //Select the new color
   $newColor.click();
 });
 
-//On mouse events on the canvas
 $canvas.mousedown(function(e){
   lastEvent = e;
   mouseDown = true;
 }).mousemove(function(e){
-  //Draw lines
   if(mouseDown) {
-    context.beginPath();
-    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-    context.lineTo(e.offsetX, e.offsetY);
-    context.strokeStyle = color;
-    context.stroke();
-    lastEvent = e;
+    if($canvas.hasClass("penCurse")){
+      context.beginPath();
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+      context.lineTo(e.offsetX, e.offsetY);
+      context.strokeStyle = color;
+      context.lineWidth = strokeWidth;
+      context.stroke();
+      lastEvent = e;
+    } else if($canvas.hasClass("eraserCurse")){
+      context.beginPath();
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+      context.lineTo(e.offsetX, e.offsetY);
+      context.strokeStyle = "#ffffff";
+      context.lineWidth = strokeWidth;
+      context.stroke();
+      lastEvent = e;
+    }
   }
 }).mouseup(function(){
   mouseDown = false;
